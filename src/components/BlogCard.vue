fdee413df5480109f754751b07f34734177739a4
kruizo <kynethruizo@gmail.com>
2025-01-06 13:51:20 +0800

    separated card to a component

---
+++ src/components/BlogCard.vue
@@ -0,0 +1,42 @@
+<template>
+  <GlowEffect>
+    <div
+      class="group flex justify-between border-2 p-2 border-white/5 rounded-2xl transition-all shadow-lg shadow-black/80 backdrop-blur-md glow:ring-1 glow:border-glow glow:ring-glow glow:bg-glow/[.15]"
+    >
+      <div class="flex flex-col mb-2 gap-1 break-all break-words">
+        <a :href="`/blog/${blog.id}/`">
+          <h3 class="text-xl font-semibold text-primary text-content">
+            {{ blog.data.title }}
+          </h3>
+        </a>
+        <p class="text-sm text-primary text-content m-0 p-0 leading-tight">
+          <span class="font-extralight text-gray-500"
+            >@{{ blog.data.author }}</span
+          >
+        </p>
+        <p
+          class="text-gray-500 text-sm m-0 p-0 text-content line-clamp-1 text-ellipsis"
+        >
+          {{ blog.data.description }}
+        </p>
+      </div>
+      <p class="font-medium text-sm text-primary text-gray-500">
+        {{ formatDate(blog.data.pubDate) }}
+      </p>
+    </div>
+  </GlowEffect>
+</template>
+
+<script>
+import GlowEffect from "./GlowEffect.vue";
+
+export default {
+  components: {
+    GlowEffect,
+  },
+  props: {
+    blog: Object,
+    formatDate: Function,
+  },
+};
+</script>
--- src/components/Content.vue
+++ src/components/Content.vue
@@ -5,46 +5,17 @@
         you might like
       </p>
     </div>
-    <ul>
-      <div class="flex flex-col gap-4">
-        <li v-for="blog in blogs" :key="blog.id">
-          <GlowEffect>
-            <div
-              class="group flex justify-between border-2 p-2 border-white/5 rounded-2xl transition-all shadow-lg shadow-black/80 backdrop-blur-md glow:ring-1 glow:border-glow glow:ring-glow glow:bg-glow/[.15]"
-            >
-              <div class="flex flex-col mb-2 gap-1 break-all break-words">
-                <a :href="`/blog/${blog.id}/`">
-                  <h3 class="text-xl font-semibold text-primary text-content">
-                    {{ blog.data.title }}
-                  </h3>
-                </a>
-                <p
-                  class="text-sm text-primary text-content m-0 p-0 leading-tight"
-                >
-                  <span class="font-extralight font text-gray-500"
-                    >@{{ blog.data.author }}</span
-                  >
-                </p>
-                <p
-                  class="text-gray-500 text-sm m-0 p-0 text-content line-clamp-1 text-ellipsis"
-                >
-                  {{ blog.data.description }}
-                </p>
-              </div>
-              <p class="font-medium text-sm text-primary text-gray-500">
-                {{ formatDate(blog.data.pubDate) }}
-              </p>
-            </div>
-          </GlowEffect>
-        </li>
-      </div>
+    <ul class="flex flex-col gap-4">
+      <li v-for="blog in blogs" :key="blog.id">
+        <BlogCard :blog="blog" :formatDate="formatDate" />
+      </li>
     </ul>
   </div>
 </template>
 
 <script>
+import BlogCard from "./BlogCard.vue";
 import { getCollection } from "astro:content";
-import GlowEffect from "./GlowEffect.vue";
 
 const blogs = (await getCollection("blog")).sort(
   (a, b) => b.data.pubDate.valueOf() - a.data.pubDate.valueOf()
@@ -58,7 +29,7 @@ const formatDate = (date) =>
 
 export default {
   components: {
-    GlowEffect, // Register GlowEffect component here
+    BlogCard,
   },
 
   setup() {
@@ -69,5 +40,3 @@ export default {
   },
 };
 </script>
-
-<style scoped></style>
